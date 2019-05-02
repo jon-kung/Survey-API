50064d9982ce185542cd3189343758fd689cd213
Jonathan <jkung92@gmail.com>
2019-05-01 17:41:21 -0700

    update dddl

--- data.sql
+++ data.sql
@@ -2,6 +2,7 @@ DROP TABLE IF EXISTS surveys;
 
 CREATE TABLE surveys (
     id SERIAL PRIMARY KEY,
+    title text,
     question text NOT NULL,
     choices BOOLEAN NOT NULL,
     answers BOOLEAN[]
