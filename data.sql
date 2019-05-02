5ce6d0455650f0ed39037fa7035c345c1190b5c6
Jonathan <jkung92@gmail.com>
2019-05-01 17:05:13 -0700

    updated DDL for answers

--- data.sql
+++ data.sql
@@ -2,8 +2,7 @@ DROP TABLE IF EXISTS surveys;
 
 CREATE TABLE surveys (
     id SERIAL PRIMARY KEY,
-    title text NOT NULL,
-    questions text NOT NULL,
+    question text NOT NULL,
     choices BOOLEAN NOT NULL,
-    answers BOOLEAN
+    answers BOOLEAN[]
 );
