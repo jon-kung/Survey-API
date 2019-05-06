e7af5bda74ab1b335529ec7b037c06b24a696b77
Jonathan <jkung92@gmail.com>
2019-05-06 14:59:20 -0700

    fixed schema

--- data.sql
+++ data.sql
@@ -1,15 +1,21 @@
 DROP TABLE IF EXISTS surveys;
+DROP TABLE IF EXISTS questions;
 DROP TABLE IF EXISTS responses;
 
 CREATE TABLE surveys (
     id SERIAL PRIMARY KEY,
-    category text,
-    question text NOT NULL,
-    choices BOOLEAN[] NOT NULL
+    survey_name text
+);
+
+CREATE TABLE questions (
+    id SERIAL PRIMARY KEY,
+    survey_id int NOT NULL FOREIGN KEY REFERENCES surveys,
+    question text
 );
 
 CREATE TABLE responses (
     id SERIAL PRIMARY KEY,
-    question_id int NOT NULL,
-    answer BOOLEAN NOT NULL 
+    question_id int NOT NULL FOREIGN KEY REFERENCES questions,
+    answer BOOLEAN 
 );
+
