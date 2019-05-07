dbaced1afc1c7c12187058ddaa60e03a5cf4697a
Jonathan <jkung92@gmail.com>
2019-05-06 17:05:23 -0700

    Using json server instead of DB

---
+++ _tests_/integration/surveys.test.js
@@ -0,0 +1,252 @@
+
+process.env.NODE_ENV = 'test';
+
+const request = require('supertest');
+const app = require('../../app');
+const db = require('../../db');
+
+
+beforeEach(async () => {
+  let companyResult = await db.query(`
+    INSERT INTO
+      companies (handle, name, num_employees, description, logo_url)
+      VALUES(
+        'testHandle2',
+        'testCompany2',
+        1000,
+        'The best test of the rest, I DO NOT JEST',
+        'https://www.url.com') RETURNING *`);
+
+  let result = await db.query(`
+    INSERT INTO 
+      jobs (id, title, salary, equity, company_handle)   
+      VALUES(
+        1,
+        'testJob', 
+        22.22, 
+        .50, 
+        'testHandle2') RETURNING *`);
+
+  let hashedPassword = await bcrypt.hash('12345', 1);
+
+  let userResult = await db.query(
+    `
+          INSERT INTO 
+            users (username,
+              password,
+              first_name,
+              last_name,
+              email,
+              photo_url,
+              is_admin)   
+            VALUES(
+              'testUsername',
+              $1,
+              'testFirstName',
+              'testLastName',
+              'test@test.com',
+              'https://www.photo.com',
+              'true') RETURNING *`,
+    [hashedPassword]
+  );
+
+  const response = await request(app)
+    .post('/users/login')
+    .send({ username: 'testUsername', password: '12345' });
+  auth.token = response.body.token;
+
+  // job = result.rows[0];
+});
+
+// TESTING route for getting all jobs
+describe('GET /jobs', async function() {
+  test('gets all jobs', async function() {
+    const response = await request(app)
+      .get(`/jobs`)
+      .send({ _token: auth.token });
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toHaveProperty('jobs');
+    expect(response.body.jobs).toHaveLength(1);
+  });
+  // Testing for no results from query
+  test('Responds with 200 if no job is found in the database', async function() {
+    await db.query('DELETE FROM jobs');
+    const response = await request(app)
+      .get(`/jobs`)
+      .send({ _token: auth.token });
+    expect(response.statusCode).toBe(200);
+    expect(response.body.jobs).toEqual([]);
+  });
+});
+
+describe('GET query string params', async function() {
+  // TESTING route for getting specific jobs with a search query
+  describe('GET /jobs?search', async function() {
+    test('gets specific job(s) with query of title', async function() {
+      const response = await request(app)
+        .get(`/jobs?search=testJob`)
+        .send({ _token: auth.token });
+      expect(response.statusCode).toBe(200);
+      expect(response.body.jobs[0].title).toBe('testJob');
+      expect(response.body.jobs).toHaveLength(1);
+    });
+    // Testing for no results from query
+    test('Responds with 200 if no job is found', async function() {
+      const response = await request(app)
+        .get(`/jobs?search=BADSEARCH`)
+        .send({ _token: auth.token });
+      expect(response.statusCode).toBe(200);
+      expect(response.body.jobs).toEqual([]);
+    });
+  });
+
+  // TESTING route to find a job with min salary of query
+  describe('GET /jobs?min_salary', async function() {
+    test('gets specific job(s) with query of min_salary', async function() {
+      const response = await request(app)
+        .get(`/jobs?min_salary=10.50`)
+        .send({ _token: auth.token });
+      expect(response.statusCode).toBe(200);
+      expect(response.body.jobs[0].title).toBe('testJob');
+    });
+    // Testing for no results from query
+    test('Responds with 200 if no job is found', async function() {
+      const response = await request(app)
+        .get(`/jobs?min_salary=100.50`)
+        .send({ _token: auth.token });
+      expect(response.statusCode).toBe(200);
+      expect(response.body.jobs).toEqual([]);
+    });
+  });
+
+  // TESTING route to find a job with min equity of query
+  describe('GET /jobs?min_equity', async function() {
+    test('gets specific job(s) with query of min_equity', async function() {
+      const response = await request(app)
+        .get(`/jobs?min_equity=0.25`)
+        .send({ _token: auth.token });
+      expect(response.statusCode).toBe(200);
+      expect(response.body.jobs[0].title).toBe('testJob');
+    });
+    // Testing for no results from query
+    test('Responds with 200 if no job is found', async function() {
+      const response = await request(app)
+        .get(`/jobs?min_equity=0.75`)
+        .send({ _token: auth.token });
+      expect(response.statusCode).toBe(200);
+      expect(response.body.jobs).toEqual([]);
+    });
+  });
+
+  // TESTING route for getting specific job with an id
+  describe('GET /jobs/id', async function() {
+    test('gets specific job with specific id', async function() {
+      const response = await request(app)
+        .get(`/jobs/1`)
+        .send({ _token: auth.token }); // job.id
+      expect(response.statusCode).toBe(200);
+      expect(response.body.job.title).toBe('testJob');
+    });
+    // Testing for failures if no job is found with id provided
+    test('Responds with 404 if no job is found with id provided', async function() {
+      const response = await request(app)
+        .get(`/jobs/BADHANDLE`)
+        .send({ _token: auth.token });
+      expect(response.statusCode).toBe(404);
+    });
+
+    // TODO- more reqs for query string - what if they search for multiple query string params?
+  });
+});
+
+/***************** END OF GET jobs tests *****************/
+/***************** BEGINNING OF POST/PATCH/DELETE jobs tests  ***************/
+
+// POST /jobs - create job from data; return {job: jobData}
+describe('POST /jobs', async function() {
+  test('creates a new job', async function() {
+    const response = await request(app)
+      .post(`/jobs`)
+      .send({
+        title: 'banana manager',
+        salary: 33.33,
+        equity: 0.75,
+        company_handle: 'testHandle2',
+        _token: auth.token
+      });
+    expect(response.statusCode).toBe(200);
+    expect(response.body.job.title).toBe('banana manager');
+    expect(response.body.job.salary).toBe(33.33);
+    expect(response.body.job.equity).toBe(0.75);
+    expect(response.body.job.company_handle).toBe('testHandle2');
+    // JSON schema validator will validate for bad user data
+  });
+  test('Responds with 400 if handle is not found', async function() {
+    const response = await request(app)
+      .post(`/jobs`)
+      .send({ company_handle: 'BADHANDLE', title: 'taco', _token: auth.token });
+    expect(response.statusCode).toBe(400);
+  });
+});
+
+// PATCH /jobs - updates job from specific handle provided in url, return {job: jobData}
+describe('PATCH /jobs/:id', async function() {
+  test('updates a job', async function() {
+    const response = await request(app)
+      .patch(`/jobs/1`)
+      .send({
+        title: 'updatedJobTitle',
+        salary: 99.99,
+        equity: 0.33,
+        company_handle: 'testHandle2',
+        _token: auth.token
+      });
+    expect(response.statusCode).toBe(200);
+    expect(response.body.job.title).toBe('updatedJobTitle');
+    expect(response.body.job.salary).toBe(99.99);
+    expect(response.body.job.equity).toBe(0.33);
+    expect(response.body.job.company_handle).toBe('testHandle2');
+    // JSON schema validator will validate for bad user data
+  });
+  test('Responds with 404 if no job is found', async function() {
+    const response = await request(app)
+      .patch(`/jobs/200`)
+      .send({
+        title: 'updatedJobTitle',
+        salary: 99.99,
+        equity: 0.33,
+        company_handle: 'testHandle2',
+        _token: auth.token
+      });
+    expect(response.statusCode).toBe(404);
+  });
+});
+
+// DELETE /jobs - deletes a job with matching id provided returning {message: "Job deleted"}
+describe('DELETE /jobs/:id', async function() {
+  test('deletes a job', async function() {
+    const response = await request(app)
+      .delete(`/jobs/1`)
+      .send({ _token: auth.token });
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual({ message: 'Job deleted' });
+  });
+  test('Responds with 404 if no company is found', async function() {
+    const response = await request(app)
+      .delete(`/jobs/BADHANDLE`)
+      .send({ _token: auth.token });
+    expect(response.statusCode).toBe(404);
+  });
+});
+/***************** END OF POST/PATCH/DELETE jobs tests *****************/
+
+// Tear Down - removes records from test DB
+afterEach(async function() {
+  await db.query('DELETE FROM users');
+  await db.query('DELETE FROM jobs');
+  await db.query('DELETE FROM companies');
+});
+
+afterAll(async function() {
+  await db.end();
+});
---
+++ api.js
@@ -0,0 +1,49 @@
+import axios from 'axios';
+const BASE_API_URL = 'http://localhost:3000';
+
+// API Should Support:
+// Creating a survey
+// Taking a Survey
+// Getting Results of a Survey
+// A survey should consist of survey questions and each question should have yes/no (true/false) answers
+
+class SurveyApi {
+  // Create a survey
+  static async addSurvey(surveyName, questions) {
+    let data = {surveyName, questions}
+    const result = await axios.post(`${BASE_API_URL}/surveys`, data, 'post');
+    return result.data;
+  }
+
+  // Show all surveys
+  static async getSurveys() {
+    const result = await axios.get(`${BASE_API_URL}/surveys`);
+    return result.data;
+  }
+  // Show one survey with all its questions
+  static async getSurveyById(surveyId){
+    const result = await axios.get(`${BASE_API_URL}/surveys?id=${surveyId}`);
+    return result.data;
+  }
+  // Take a survey - adds data to responses
+  static async takeSurvey(questionId, answer) {
+    let data = {questionId, answer}
+    const result = await axios.post(`${BASE_API_URL}/responses`, data, 'post');
+    return result.data;
+  }
+
+  // Shows all survey responses
+  static async getAllResults() {
+    const result = await axios.get(`${BASE_API_URL}/responses`);
+    return result.data;
+  }
+
+  // Shows a specific response by survey id
+  static async getResult(surveyId) {
+    const result = await axios.get(`${BASE_API_URL}/responses?survey_id=${surveyId}`);
+    return result.data;
+  }
+
+}
+
+export default SurveyApi;
