7cac4ce77ba3c746fb8ac972f8839931d9db4a2f
Jonathan <jkung92@gmail.com>
2019-05-17 17:11:24 -0700

    added test data

--- data.sql
+++ data.sql
@@ -19,3 +19,15 @@ CREATE TABLE responses (
     answer BOOLEAN 
 );
 
+INSERT INTO surveys (id, survey_name) VALUES( 1, 'First Survey' );
+INSERT INTO surveys (id, survey_name) VALUES( 2, 'Second Survey' );
+
+INSERT INTO questions (id, survey_id, question) VALUES( 1, 1, 'Do you like testing?');
+INSERT INTO questions (id, survey_id, question) VALUES( 2, 1, 'Do you like not testing?');
+INSERT INTO questions (id, survey_id, question) VALUES( 3, 2, 'Do you like cake?');
+INSERT INTO questions (id, survey_id, question) VALUES( 4, 2, 'Do you like pie?');
+
+INSERT INTO responses (id, question_id, answer) VALUES( 1, 1, false );
+INSERT INTO responses (id, question_id, answer) VALUES( 2, 2, true );
+INSERT INTO responses (id, question_id, answer) VALUES( 3, 3, true );
+INSERT INTO responses (id, question_id, answer) VALUES( 4, 4, true );
